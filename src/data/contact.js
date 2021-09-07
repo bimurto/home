20159e40f818995c6eb90999865ee59b6ff9f1f5
Shawrup <shaawrup.k.suter@gmail.com>
2021-09-08 01:35:22 +0600

    Updated picture and contacts

---
+++ src/data/contact.js
@@ -0,0 +1,31 @@
+import {faGithub} from '@fortawesome/free-brands-svg-icons/faGithub';
+import {faLinkedinIn} from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons/faEnvelope';
+import {faStackOverflow} from "@fortawesome/free-brands-svg-icons/faStackOverflow";
+// See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
+// to add other icons.
+
+const data = [
+  {
+    link: 'https://github.com/bimurto',
+    label: 'Github',
+    icon: faGithub,
+  },
+  {
+    link: 'https://www.linkedin.com/in/shawrup-kumer-suter/',
+    label: 'LinkedIn',
+    icon: faLinkedinIn,
+  },
+  {
+    link: 'https://stackoverflow.com/users/13755857/shawrup',
+    label: 'Stackoverflow',
+    icon: faStackOverflow,
+  },
+  {
+    link: 'mailto:shawrup.k.suter@gmail.com',
+    label: 'Email',
+    icon: faEnvelope,
+  },
+];
+
+export default data;
---
+++ src/pages/Contact.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Main from '../layouts/Main';
+import EmailLink from '../components/Contact/EmailLink';
+import ContactIcons from '../components/Contact/ContactIcons';
+
+const Contact = () => (
+  <Main
+    title="Contact"
+    description="Contact Shawrup K. Suter via email @ shawrup.k.suter@gmail.com"
+  >
+    <article className="post" id="contact">
+      <header>
+        <div className="title">
+          <h2 data-testid="heading"><Link to="/contact">Contact</Link></h2>
+        </div>
+      </header>
+      <div className="email-at">
+        <p>Feel free to get in touch. You can email me at: <a href="mailto:shawrup.k.suter@gmail.com">shawrup.k.suter@gmail.com</a></p>
+      </div>
+      <ContactIcons />
+    </article>
+  </Main>
+);
+
+export default Contact;
